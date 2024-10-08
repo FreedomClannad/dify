220e3ff1eb04edb2d6405b3f260b4098b64d34b6
freedom <459566948@qq.com>
2024-10-08 19:13:14 +0800

    Receptor上传文件处理

--- web/app/(commonLayout)/tools/docking/Result/DockingInputFile.tsx
+++ web/app/(commonLayout)/tools/docking/Result/DockingInputFile.tsx
@@ -1,22 +1,40 @@
 import { useContext, useMemo } from 'react'
+import { DocumentTextIcon } from '@heroicons/react/24/outline'
+import { RiEyeLine, RiEyeOffLine } from '@remixicon/react'
 import VerticalTitleCard from '@/app/components/card/vertical-title-card'
 import { ResultContext } from '@/app/(commonLayout)/tools/docking/Result/context'
 import type { DockingUploadFile } from '@/types/docking'
 import { MolstarContext } from '@/app/(commonLayout)/tools/docking/context/molstar'
 
+type CardType = {
+  docking: DockingUploadFile
+  onClick: (dockingUploadFile: DockingUploadFile) => void
+}
+const Card = ({ docking, onClick }: CardType) => {
+  return <>
+    <div className="flex w-full justify-between">
+      <div className="flex">
+        <div className='w-5 h-5  text-xs'><DocumentTextIcon/></div>
+        <span>{docking.file.name}</span>
+      </div>
+      <div className="cursor-pointer" onClick={() => {
+        onClick(docking)
+      }}>{docking.visible ? <RiEyeLine/> : <RiEyeOffLine/>}</div>
+    </div>
+  </>
+}
+
 const DockingInputFile = () => {
   const { receptorFileList } = useContext(ResultContext)
-  const { dockingMolstarList } = useContext(MolstarContext)
+  const { dockingMolstarList, setStructureVisibility } = useContext(MolstarContext)
   const receptorVisibleFileList: DockingUploadFile[] = useMemo(() => {
     const n_list: DockingUploadFile[] = []
-    console.log(receptorFileList)
-    console.log(dockingMolstarList)
     receptorFileList.map((item) => {
       const docking = dockingMolstarList.find(dockingItem => dockingItem.id === item.fileID)
       if (docking) {
         n_list.push({
           ...item,
-          visible: true,
+          visible: docking.visible,
         },
         )
       }
@@ -25,9 +43,17 @@ const DockingInputFile = () => {
     })
     return n_list
   }, [dockingMolstarList, receptorFileList])
+  const handleClick = (dockingFile: DockingUploadFile) => {
+    console.log(dockingFile)
+    setStructureVisibility({
+      dockingMolstar: { id: dockingFile.fileID, visible: !dockingFile.visible },
+    })
+  }
   return <VerticalTitleCard title="Docking input file">
     <div className="w-full">{
-      receptorVisibleFileList.map((item, index) => <div key={index}>{item.file.name}</div>)
+      receptorVisibleFileList.map((item, index) => {
+        return <Card key={`receptro-${index}`} docking={item} onClick={handleClick} />
+      })
     }</div>
   </VerticalTitleCard>
 }
