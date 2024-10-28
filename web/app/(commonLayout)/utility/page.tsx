846363c328a5b97b1ccf3989f0fce2ec497d9ebd
freedom <459566948@qq.com>
2024-10-28 13:54:00 +0800

    utility的初步代码

---
+++ web/app/(commonLayout)/utility/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import type { FC } from 'react'
+import React from 'react'
+import UtilityList from '@/app/components/utility/utility-list'
+const Page: FC = () => {
+  return <UtilityList />
+}
+
+export default React.memo(Page)
